61c42938f1b5679f3e82f8b71ce5139a325cbd33
cuddleminddev <cuddleminddev@gmail.com>
2025-07-14 18:49:48 +0530

    header correction

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -23,8 +23,8 @@ export default function Header() {
         <nav className={menuOpen ? 'open' : ''}>
           <ul>
             <li><Link to="/" onClick={() => setMenuOpen(false)}>Home</Link></li>
-            <li><Link to="/about" onClick={() => setMenuOpen(false)}>About</Link></li>
             <li><Link to="/courses" onClick={() => setMenuOpen(false)}>Courses</Link></li>
+            <li><Link to="/about" onClick={() => setMenuOpen(false)}>About</Link></li>
             <li><Link to="/contact" onClick={() => setMenuOpen(false)}>Contact</Link></li>
           </ul>
         </nav>
