10bb281bc1283a3b1fa8be2105bbb6441b72c573
cuddleminddev <cuddleminddev@gmail.com>
2025-07-14 15:41:50 +0530

    basic hero section

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,9 @@
+import Hero from "./home/hero";
+
 export default function Home() {
   return (
-    <div className="page">
-      <h1>Home Page</h1>
-      <p>Welcome to our website!</p>
+    <div>
+      <Hero />
     </div>
   );
 }
---
+++ src/pages/home/hero.jsx
@@ -0,0 +1,30 @@
+import './styles/hero.css'
+
+export default function Hero() {
+    return (
+        <div className="hero-container">
+            <div className="hero-left">
+                <h1>Master New Skills</h1>
+                <h1>In Just 2 Months</h1>
+                <p>No boring theory. Learn with real projects, get hands-on experience, and build your future with industry-expert guidance.</p>
+                <button>Start Learning Today<i className="fa-solid fa-arrow-right"></i></button>
+                <div className="hero-list">
+                    <ul>
+                        <li><i className="fa-regular fa-circle-check"></i> Industry-Ready Skills</li>
+                        <li><i className="fa-regular fa-circle-check"></i> Expert Mentorship</li>
+                        <li><i className="fa-regular fa-circle-check"></i> Lifetime Support</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="hero-right">
+                <div className="main-img-container">
+                    <span className="job-badge float-behind">100% Job Support</span>
+                    <img src="/ins1.jpg" alt="Instructor" className="main-img" />
+                    <span className="expert-badge">Expert Instructors</span>
+                </div>
+                <img src="/ins2.jpg" alt="Instructor" className="small-img" />
+            </div>
+
+        </div>
+    )
+}
