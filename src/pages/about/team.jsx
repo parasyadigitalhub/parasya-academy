d3692f41dd796af67382f470cc5ef05004374bce
cuddleminddev <cuddleminddev@gmail.com>
2025-08-04 10:24:03 +0530

    team updated

--- src/pages/about/team.jsx
+++ src/pages/about/team.jsx
@@ -3,27 +3,34 @@ import './styles/team.css'
 const team = [
     {
         id: 1,
-        icon: '<i class="fa-solid fa-bullseye"></i>',
-        name: 'Ashwin',
+        icon: '<i class="fa-solid fa-user-tie"></i>',
+        name: 'Arjun',
         position: 'Founder',
         para: 'Visionary leader with extensive experience in performance marketing and digital strategy.'
     },
     {
         id: 2,
+        icon: '<i class="fa-solid fa-bullseye"></i>',
+        name: 'Ashwin PP',
+        position: 'Co-Founder',
+        para: 'Expert in strategic growth and partnerships, driving product-market fit and scaling operations from seed to Series A.'
+    },
+    {
+        id: 3,
         icon: '<i class="fa-regular fa-user"></i>',
         name: 'Rishan',
         position: 'Lead Mentor',
         para: 'Performance marketing expert with proven track record in scaling campaigns.'
     },
     {
-        id: 3,
+        id: 4,
         icon: '<i class="fa-solid fa-medal"></i>',
         name: 'Reema',
         position: 'Mentor',
         para: 'Digital marketing specialist focused on conversion optimization and analytics.'
     },
     {
-        id: 4,
+        id: 5,
         icon: '<i class="fa-solid fa-heart"></i>',
         name: 'Dart Coder',
         position: 'Technical Mentor',
